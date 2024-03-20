d5cb3d69e6856d4e5ae18ca1ce91926a06fb3761
Nishant6692 <nishantkaushal692@gmail.com>
2024-03-21 00:41:37 +0530

    otpimize code

--- app/Services/MyTotalPay.php
+++ app/Services/MyTotalPay.php
@@ -1,33 +1,42 @@
 <?php
+
 namespace App\Services;
+
 use GuzzleHttp\Client;
-final class MyTotalPay{
- protected $options;
- protected $url;
- public function __construct(string $url,array $options){
-    $this->options = $options;
-    $this->url = $url;
-}
 
-/**
- * getCheckOutUrl function for getting the checkout url 
- *
- * @return string url 
- */
-public function getCheckOutUrl(){
-    $data =  $this->options;
-    $url = $this->url;
-   $client = new Client();
-   $response = $client->request('POST',$url,[
-    'body'=> json_encode($data),
-    'headers' => [
-        'Content-Type' => 'application/json',
-    ],
-   ]);
-    
-   return json_decode($response->getBody()->getContents());
-}
+final class MyTotalPay
+{
+    protected $options;
+    protected $url;
 
+    public function __construct(string $url, array $options)
+    {
+        $this->options = $options;
+        $this->url = $url;
+    }
 
+    /**
+     * getCheckOutUrl function for getting the checkout url 
+     *
+     * @return string url 
+     */
+    public function getCheckOutUrl()
+    {
+        $client = new Client();
 
+        try {
+            $response = $client->request('POST', $this->url, [
+                'body'=> json_encode($this->options),
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                ],
+               ]);
+
+            return json_decode($response->getBody()->getContents());
+        } catch (\Exception $e) {
+            // Handle any exceptions here
+            // For example: Log the error or throw a custom exception
+            return null;
+        }
+    }
 }
